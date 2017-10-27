1733d1bc8c2f98c724b0df7a3c7310c72c10f5c1
b3n10 <francisben.lleve@gmail.com>
2017-10-27 14:41:36 +0800

    Convert to Constructor Function

--- app.js
+++ app.js
@@ -1,63 +1,55 @@
 "use strict";
 
-let TypeTest = function() {
-	let txt = document.getElementsByClassName("text")[0];
-	let hnt = document.getElementsByClassName("hint")[0];
-	let sp = 0, tp = 0;
+let TypeTest = (function() {
+	function TypeTest() {
+		this.txt = document.getElementsByClassName("text")[0];
+		this.hnt = document.getElementsByClassName("hint")[0];
+		this.sp = 0, this.tp = 0;
+		this.scripts = "Domain name lookup service to search the whois database for domain name registration information.";//should be taken from a json file, xml, or database
+		this.hint();
+	}
+	TypeTest.prototype.hint = function(_status) {
+		this.txt.innerHTML = this.highlight(this.scripts);
+		this.hnt.innerHTML = (_status) ? _status : ("Next Character: " + ((this.scripts[0] === " ") ? "\< space \>" : this.scripts[0]));
+	}
+	TypeTest.prototype.highlight = function(_string) {
+		let text = "<span class='highlight'>";
+		let i = 0;
 
-	let TTobj = {
-		scripts: "wHat kind of portfolio projects do you think would a career shifter new to web development would impress employers or make someone hirable, give opinion for both front and backend please ",//should be taken from a json file, xml, or database
+		while (_string[i] !== undefined && _string[i] !== ' ') {
+			text += _string[i];
+			i++;
+		}
 
-		hint: function(_status) {
-			//console.log(event.key === this.scripts[0]);
-			txt.innerHTML = this.highlight(this.scripts);
-			hnt.innerHTML = (_status) ? _status : ("Next Character: " + ((this.scripts[0] === " ") ? "\< space \>" : this.scripts[0]));
-		},
+		text += "</span>";
+		text += _string.slice(i, _string.length);
 
-		highlight: function(_string) {
-			let text = "<span class='highlight'>";
-			let i = 0;
+		return text;
+	}
+	TypeTest.prototype.score = function() {
+		this.tp++;
+		if (event.key === this.scripts[0]) {
+			this.sp++;
+		}
+		return "Accuracy: " + ((this.sp/this.tp) * 100).toFixed(2) + "%";
+	}
+	TypeTest.prototype.keyDown = function() {
+		if (event.key !== "Control" && event.key !== "Shift" && event.key !== "Meta" && event.key !== "Alt" && event.key !== "ArrowLeft" && event.key !== "ArrowUp" && event.key !== "ArrowDown" && event.key !== "ArrowRight") {
+			let s = this.score();
+			this.scripts = this.scripts.slice(1, this.scripts.length);//remove first character
 
-			while (_string[i] !== undefined && _string[i] !== ' ') {
-				text += _string[i];
-				i++;
-			}
-
-			text += "</span>";
-			text += _string.slice(i, _string.length);
-
-			return text;
-		},
-
-		score: function() {
-			tp++;
-			if (event.key === this.scripts[0]) {
-				sp++;
-			}
-			return "Accuracy: " + ((sp/tp)*100).toFixed(2) + "%";
-		},
-
-		keyDown: function() {
-			if (event.key !== "Control" && event.key !== "Shift" && event.key !== "Meta" && event.key !== "Alt" && event.key !== "ArrowLeft" && event.key !== "ArrowUp" && event.key !== "ArrowDown" && event.key !== "ArrowRight") {
-				let s = this.score();
-				this.scripts = this.scripts.slice(1, this.scripts.length);//remove first character
-
-				//check if done
-				if (this.scripts.length === 0) {
-					this.hint("Done !<br>" + s);
-					window.removeEventListener("keydown", kd, true);
-				} else {
-					this.hint();
-				}
+			//check if done
+			if (this.scripts.length === 0) {
+				this.hint("Done !<br>" + s);
+				window.removeEventListener("keydown", kd, true);
+			} else {
+				this.hint();
 			}
 		}
 	}
+	return TypeTest;
+}());
 
-	return TTobj;
-}
-
-let tt = TypeTest();
-tt.hint();//run once to see next char
+let tt = new TypeTest();
 let kd = tt.keyDown.bind(tt);
 window.addEventListener("keydown", kd, true);
-
